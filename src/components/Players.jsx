0c8ee646ce2c9ab8f025401ee4391f5a9ecd459a
Szymon Samus <siizmonsus@gmail.com>
2024-03-20 16:04:37 +0100

    Change: Edited Player function in order to clean up the JSX (Switched from a ternary conditions to an IF statement)

--- src/components/Players.jsx
+++ src/components/Players.jsx
@@ -1,18 +1,27 @@
 import { useState } from "react";
 
 export default function Player({ name, symbol }) {
- const [ isEditing, setIsEditing ] = useState(false);
+    const [isEditing, setIsEditing] = useState(false);
 
- function handleClickEdit() {
-        setIsEditing(!isEditing);    
- }
-    return (
-        <li>
-            <span className="player">
-            {isEditing ? (<input></input>) : <span className="player-name">{name}</span>}
-              <span className="player-symbol">{symbol}</span>
-            </span>
-            <button onClick={handleClickEdit}>Edit</button>
-          </li>
-    );
+    function handleClickEdit() {
+        setIsEditing(!isEditing);
+    }
+
+    let playerName = <span className="player-name">{name}</span>;
+    let btnCaption = "Edit"
+
+    if (isEditing) {
+        playerName = <input type="text" required />;
+        btnCaption = "Save";
+    }
+
+        return (
+            <li>
+                <span className="player">
+                    {playerName}
+                    <span className="player-symbol">{symbol}</span>
+                </span>
+                <button onClick={handleClickEdit}>{btnCaption}</button>
+            </li>
+        );
 }
