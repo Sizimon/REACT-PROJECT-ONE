0fb031a1aded13192c480044ea80ce89d54e1874
Szymon Samus <siizmonsus@gmail.com>
2024-03-22 16:06:55 +0100

    Added: Functionality to alternate between players after a player has made their turn. Along with UI which shows which player is currently making their turn.

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -6,15 +6,17 @@ const initialGameBoard = [
     [null, null, null]
 ];
 
-export default function GameBoard() {
+export default function GameBoard({ onSelectSquare, activePlayerSymbol }) {
     const [gameBoard, setGameBoard] = useState(initialGameBoard);
 
     function handleSelectSquare(rowIndex, cellIndex) {
         setGameBoard((prevGameBoard) => {
             const updatedBoard = [...prevGameBoard.map(innerArray => [...innerArray])];
-            updatedBoard[rowIndex][cellIndex] = 'X';
+            updatedBoard[rowIndex][cellIndex] = activePlayerSymbol;
             return updatedBoard;
         });
+
+        onSelectSquare();
     }
 
     return (
