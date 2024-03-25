a262c8ea87fd69ba23208fa054f1afe0ba41244a
Szymon Samus <siizmonsus@gmail.com>
2024-03-25 22:07:06 +0100

    COMPLETE: Final fixes including adding capability to display player inserted name in the log + win box.

--- src/components/Log.jsx
+++ src/components/Log.jsx
@@ -2,7 +2,7 @@ export default function Log({ turns }) {
     return (
         <ol id='log'>
             {turns.map(turn => (
-            <li key={`${turn.square.row}${turn.square.col}`}>
+            <li key={`${turn.square.row}${turn.square.cell}`}>
                 {turn.player} selected {turn.square.row},{turn.square.cell}
                 </li>
             ))}
